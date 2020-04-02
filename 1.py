99ceb29324d262bbbdf686ae60311c03cbf2625e
AlexandrMihaylin <alexandr.mihaylin@gmail.com>
2020-04-03 00:35:49 +0400

    lesson1 от 31.03.20202

---
+++ 1.py
@@ -0,0 +1,14 @@
+#Поработайте с переменными, создайте несколько, выведите на экран,
+# запросите у пользователя несколько чисел и строк и сохраните в переменные, выведите на экран.
+
+number = 77
+word = 'аз'
+pravda = False
+print(number, '=', type(number))
+print(word, '=', type(word))
+print(pravda, '=', type(pravda))
+
+your_number = int(input('Введите Ваше число, пожалуйста! - '))
+your_word = input('Введите Ваше слово, пожалуйста! - ')
+print(your_number, '- Ваше число')
+print(your_word, 'Ваше слово.')
